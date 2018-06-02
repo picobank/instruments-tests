d91f422479aa5ec75b580ea007d4560584167a94
Jacques Desmazieres <jacques.desmazieres@gmail.com>
2018-06-02 20:47:46 +0200

    améliorations diverses

--- dao-pgx/dao-pgx.go
+++ dao-pgx/dao-pgx.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strconv"
+	"strings"
 	"time"
 
 	// https://github.com/jackc/pgx
@@ -17,14 +17,16 @@ var pool *pgx.ConnPool
 
 const instrumentCols string = "i.instrument_id, i.symbol, i.name, i.description, i.instrument_class_id, i.currency_id, i.from_date, i.thru_date, i.created_at, i.created_by, i.updated_at, i.updated_by"
 const instrumentClassCols string = "ic.instrument_class_id, ic.name"
-const getInstrumentByID string = "select " + instrumentCols + ", " + instrumentClassCols + " from instrument i join instrument_class ic on ic.instrument_class_id = i.instrument_class_id where instrument_id = $1"
+const getInstrumentByID string = "select " + instrumentCols + ", " + instrumentClassCols + `
+	from instrument i
+	join instrument_class ic on ic.instrument_class_id = i.instrument_class_id 
+	left join instrument i2 on i2.instrument_id = i.currency_id 
+	where i.instrument_id = $1`
 const getInstrumentClassByID string = "select " + instrumentClassCols + " from instrument_class ic where instrument_class_id = $1"
 
-const searchInstruments string = "select " + instrumentCols + ", " + instrumentClassCols + " from instrument i join instrument_class ic on ic.instrument_class_id = i.instrument_class_id where 1=1"
+const searchInstruments string = "select " + instrumentCols + ", " + instrumentClassCols + " \nfrom instrument i \njoin instrument_class ic on ic.instrument_class_id = i.instrument_class_id \nwhere 1=1"
 
 const listInstrumentClasses string = "select instrument_class_id, name from instrument_class"
-const listInstruments string = "select " + instrumentCols + " from instrument i"
-const listInstrumentsForClass string = "select " + instrumentCols + " from instrument i where instrument_class_id = $1"
 
 func init() {
 	fmt.Println("\nTest package github.com/jackc/pgx ...")
@@ -150,7 +152,7 @@ func SearchInstruments(criteria *InstrumentSearchCriteria) ([]m.Instrument, erro
 	fmt.Printf("\nSearchInstruments(%v) ...\n", criteria)
 
 	query := buildCriteria(searchInstruments, criteria)
-	fmt.Printf("\t[SQL] %s (%v)\n", query, criteria)
+	fmt.Printf("\t[SQL] %s\n", query)
 	rows, err := connection().Query(query)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error executing query:", err)
@@ -169,55 +171,20 @@ func SearchInstruments(criteria *InstrumentSearchCriteria) ([]m.Instrument, erro
 }
 
 func buildCriteria(query string, criteria *InstrumentSearchCriteria) string {
-	if criteria.InstrumentID != 0 {
-		query = query + fmt.Sprintf(" and i.instrument_id = %d", criteria.InstrumentID)
+	if criteria.InstrumentID != nil && len(criteria.InstrumentID) > 0 {
+		query = query + fmt.Sprintf("\n and i.instrument_id in (%s)", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(criteria.InstrumentID)), ","), "[]"))
 	}
 	if criteria.Symbol != "" {
-		query = query + fmt.Sprintf(" and i.symbol = '%s'", criteria.Symbol)
+		query = query + fmt.Sprintf("\n and i.symbol = '%s'", criteria.Symbol)
 	}
 	if criteria.Name != "" {
-		query = query + fmt.Sprintf(" and i.name like '%%%s%%'", criteria.Name)
+		query = query + fmt.Sprintf("\n and i.name like '%%%s%%'", criteria.Name)
 	}
 	if criteria.ClassName != "" {
-		query = query + fmt.Sprintf(" and ic.name = '%s'", criteria.ClassName)
+		query = query + fmt.Sprintf("\n and ic.name = '%s'", criteria.ClassName)
 	}
 	// TODO Currency,CheckDate
 	return query
-}
-
-// ListInstrumentsForInstrumentClassID affiche la liste des instruments pour une classe dans la console
-func ListInstrumentsForInstrumentClassID(instrumentClassID int32) (int32, error) {
-	fmt.Printf("\nListe des instruments pour la classe [%d] ...", instrumentClassID)
-	fmt.Println("\n", listInstrumentsForClass)
-
-	rows, err := connection().Query(listInstrumentsForClass, instrumentClassID)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error executing query:", err)
-		os.Exit(1)
-	}
-	defer rows.Close()
-
-	var count int32
-	for rows.Next() {
-		var instrumentID, instrumentClassID int32
-		var currencyID *int32 // nillable value
-		var symbol, name, description, createdBy, updatedBy string
-		var fromDate, thruDate, createdAt, updatedAt time.Time
-
-		err := rows.Scan(&instrumentID, &symbol, &name, &description, &instrumentClassID, &currencyID, &fromDate, &thruDate, &createdAt, &createdBy, &updatedAt, &updatedBy)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error fetching query result:", err)
-			return 0, err
-		}
-		var ccy string
-		if currencyID != nil {
-			ccy = strconv.Itoa(int(*currencyID))
-		}
-		fmt.Printf("\tinstrumentID=%2d symbol='%s' instrumentClassID=%d currencyID=%s fromDate=%s thruDate=%s name='%s' description='%s' \n", instrumentID, symbol, instrumentClassID, ccy, fromDate.Format("02.01.2006"), thruDate.Format("02.01.2006"), name, description)
-		count++
-	}
-
-	return count, rows.Err()
 }
 
 func connection() *pgx.Conn {
@@ -237,7 +204,7 @@ func release(conn *pgx.Conn) {
 
 // InstrumentSearchCriteria blabla
 type InstrumentSearchCriteria struct {
-	InstrumentID uint32
+	InstrumentID []uint32
 	Symbol       string
 	Name         string
 	ClassName    string
--- http-service/http-service.go
+++ http-service/http-service.go
@@ -19,12 +19,6 @@ func listInstrumentClass(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 	fmt.Fprintf(w, " => %v\n", instrumentClasses)
 }
 
-func listInstrumentsForInstrumentClassID(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	classID, _ := strconv.Atoi(ps.ByName("classId"))
-	count, _ := p.ListInstrumentsForInstrumentClassID(int32(classID))
-	fmt.Fprintf(w, " => %d rows\n", count)
-}
-
 func httpTest(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	label := ps.ByName("label")
 	fmt.Fprintf(w, "Hello %s\n", label)
@@ -43,8 +37,8 @@ func getInstrument(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 
 func searchInstruments(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// queryValues := r.URL.Query()
-	// criteria := p.InstrumentSearchCriteria{InstrumentID: 10001}
-	criteria := p.InstrumentSearchCriteria{Name: "Dollar"}
+	criteria := p.InstrumentSearchCriteria{InstrumentID: []uint32{10001, 10002, 10005}, Name: "Dollar"}
+	// criteria := p.InstrumentSearchCriteria{Name: "Dollar"}
 	result, _ := p.SearchInstruments(&criteria)
 	b, _ := json.MarshalIndent(result, "", "  ")
 	fmt.Fprintf(w, "%v", string(b)) // json generic formatting "encoding/json"
@@ -61,8 +55,6 @@ func Start() {
 	router.GET("/instrument/:instrumentID/", getInstrument)
 	// liste des instruments
 	router.GET("/instrument/", searchInstruments)
-	// liste des instruments d'une classe
-	//router.GET("/instrument/:classId/", listInstrumentsForInstrumentClassID)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
